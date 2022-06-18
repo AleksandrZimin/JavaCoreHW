f9645dda48393f3350ab95ca9c7c09d3bbc5253d
AleksandrZimin <uzao-rec@yandex.ru>
2022-06-19 02:43:51 +0300

    Домашнее задание 4. Доделать!

--- src/Task2/main.java
+++ src/Task2/main.java
@@ -3,15 +3,15 @@ package Task2;
 public class main {
     public static void main(String[] args) {
         PhoneBook phoneTableClass = new PhoneBook();
-        phoneTableClass.add("+74951351313","asd");
-        phoneTableClass.add("+74953566422","asd");
-        phoneTableClass.add("+79991183789","asd");
-        phoneTableClass.add("+79858092559","vbn");
-        phoneTableClass.add("+79262547986","gft");
-        phoneTableClass.add("+74994446232","wer");
-        phoneTableClass.add("+79067752214","sdf");
+        phoneTableClass.add("+74951351313","Bob Jackson");
+        phoneTableClass.add("+74953566422","Bill Tailor");
+        phoneTableClass.add("+79991183789","Sandra Donovan");
+        phoneTableClass.add("+79858092559","Villa Lover");
+        phoneTableClass.add("+79262547986","Adam Bykov");
+        phoneTableClass.add("+74994446232","Loid Frost");
+        phoneTableClass.add("+79067752214","Jack Mauser");
 
-        System.out.println(phoneTableClass.getPhoneByFIO("asd"));
-        System.out.println(phoneTableClass.getPhoneByFIO("999"));
+        System.out.println(phoneTableClass.getPhoneByFIO("Adam Bykov"));
+        System.out.println(phoneTableClass.getPhoneByFIO("79262547986"));
     }
 }
